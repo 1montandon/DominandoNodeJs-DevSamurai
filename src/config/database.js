54bf3b140bd875fb5a7ef8bae33b2abf96941690
eduardo montandon <emontandon11@gmail.com>
2025-03-31 17:33:24 -0300

    Changin depedencies

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,13 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('Local', 'postgres', '123', {
-host: 'localhost',
-dialect: 'postgres',
-logging: false,
-});
-
-(async () => {
-try {
-    await sequelize.authenticate();
-    console.log('Conectado ao banco de dados!');
-} catch (error) {
-    console.error(' Erro ao conectar ao banco:', error);
-}
-})();
-
-module.exports = sequelize;
+module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "123",
+    database: "postgres",
+    define: {
+      timestamp: true, //cria duas colunas: createdAt e UpdatedAt
+      underscored: true, //nomeclatura _ => customersGroup - custumers_group
+      undescoredAll: true,
+    },
+  };
+  
